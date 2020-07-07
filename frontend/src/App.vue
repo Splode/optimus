b323b81ac41fbcba08ab84380cf00aeccabf8439
Christopher Murphy <flyweight@pm.me>
2020-07-07 10:08:24 -0600

    initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <img alt="Wails logo" src="./assets/images/logo.png" class="logo zoomIn">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import "./assets/css/main.css";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  }
+};
+</script>
---
+++ go.mod
@@ -0,0 +1,8 @@
+module optimus
+
+go 1.14
+
+require (
+	github.com/leaanthony/mewn v0.10.7
+	github.com/wailsapp/wails v1.7.1
+)
