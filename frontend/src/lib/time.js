62d8f7ca849151e66060c13e6a33dab9ba22c311
Christopher Murphy <flyweight@pm.me>
2020-07-16 08:54:45 -0600

    feat: prettyTime function supports milliseconds

--- frontend/src/lib/time.js
+++ frontend/src/lib/time.js
@@ -4,18 +4,20 @@
  * @returns {(string)[]}
  */
 export function prettyTime(ms) {
-  const seconds = (ms / 1000).toFixed(1)
-  const minutes = (ms / (1000 * 60)).toFixed(1)
-  const hours = (ms / (1000 * 60 * 60)).toFixed(1)
-  const days = (ms / (1000 * 60 * 60 * 24)).toFixed(1)
+    const seconds = (ms / 1000).toFixed(1)
+    const minutes = (ms / (1000 * 60)).toFixed(1)
+    const hours = (ms / (1000 * 60 * 60)).toFixed(1)
+    const days = (ms / (1000 * 60 * 60 * 24)).toFixed(1)
 
-  if (seconds < 60) {
-    return [seconds, 'Seconds']
-  } else if (minutes < 60) {
-    return [minutes, 'Minutes']
-  } else if (hours < 24) {
-    return [hours, 'Hours']
-  } else {
-    return [days, 'Days']
-  }
+    if (ms < 1000) {
+        return [ms, 'Milliseconds']
+    } else if (seconds < 60) {
+        return [seconds, 'Seconds']
+    } else if (minutes < 60) {
+        return [minutes, 'Minutes']
+    } else if (hours < 24) {
+        return [hours, 'Hours']
+    } else {
+        return [days, 'Days']
+    }
 }
