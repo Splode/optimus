c216760910c9c986280fc0c8c1da989bb981aa1a
Christopher Murphy <flyweight@pm.me>
2020-07-21 09:32:50 -0600

    feat: convert PNG quality factor

--- lib/png/png.go
+++ lib/png/png.go
@@ -8,7 +8,7 @@ import (
 	"io"
 )
 
-// TODO: pass options to compressor
+const qMax = 20
 
 // Options represent PNG encoding options.
 type Options struct {
@@ -26,7 +26,14 @@ func DecodePNG(r io.Reader) (image.Image, error) {
 
 // EncodePNG encodes an image into PNG and returns a buffer.
 func EncodePNG(i image.Image, o *Options) (buf bytes.Buffer, err error) {
-	c := lossypng.Compress(i, -1, o.Quality)
+	c := lossypng.Compress(i, 2, qualityFactor(o.Quality))
 	err = png.Encode(&buf, c)
 	return buf, err
+}
+
+// qualityFactor normalizes the PNG quality factor from a max of 20, where 0 is
+// no conversion.
+func qualityFactor(q int) int {
+	f := q / 100
+	return qMax - (f * qMax)
 }
